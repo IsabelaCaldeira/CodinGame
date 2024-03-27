fadb0123742fbda373c1ef10e4c2cb5f1d20a17a
Isabela Caldeira <isabela.ribeiro@outlook.com>
2024-03-27 12:19:40 +0100

    updating logicGates in c

--- easy/logicGates/logicGates.c
+++ easy/logicGates/logicGates.c
@@ -1,58 +1,71 @@
 #include <stdio.h>
 #include <string.h>
 
-#define MAX_SIGNAL_LENGTH 100
-#define MAX_SIGNALS 16
+#define MAX_INPUTS 100
+#define MAX_NAME_LEN 50
+#define MAX_SIGNAL_LEN 100
 
-char logic_gate(char operation, char input1, char input2) {
-    switch(operation) {
-        case 'A':
-            return (input1 == '-' && input2 == '-') ? '-' : '_';
-        case 'O':
-            return (input1 == '-' || input2 == '-') ? '-' : '_';
-        case 'X':
-            return (input1 != input2) ? '-' : '_';
-        case 'N':
-            return (input1 == '-' && input2 == '-') ? '_' : '-';
-        default:
-            return (input1 == input2) ? '_' : '-';
+typedef struct {
+    char name[MAX_NAME_LEN];
+    char signal[MAX_SIGNAL_LEN];
+} Input;
+
+typedef struct {
+    char name[MAX_NAME_LEN];
+    char signal[MAX_SIGNAL_LEN];
+    char gate_type[MAX_NAME_LEN];
+    char input_name_1[MAX_NAME_LEN];
+    char input_name_2[MAX_NAME_LEN];
+} Output;
+
+char logic_gate(char* operation, char input1, char input2) {
+    if (strcmp(operation, "AND") == 0)
+        return (input1 == '-' && input2 == '-') ? '-' : '_';
+    else if (strcmp(operation, "OR") == 0)
+        return (input1 == '-' || input2 == '-') ? '-' : '_';
+    else if (strcmp(operation, "XOR") == 0)
+        return (input1 != input2) ? '-' : '_';
+    else if (strcmp(operation, "NAND") == 0)
+        return (input1 == '-' && input2 == '-') ? '_' : '-';
+    else if (strcmp(operation, "NOR") == 0)
+        return (input1 == '-' || input2 == '-') ? '_' : '-';
+    else if (strcmp(operation, "NXOR") == 0)
+        return (input1 != input2) ? '_' : '-';
+    else
+        return '0';
+}
+
+char* find_signal(Input* inputs, int n, char* name) {
+    for (int i = 0; i < n; i++) {
+        if (strcmp(inputs[i].name, name) == 0) {
+            return inputs[i].signal;
+        }
     }
+    return NULL;
 }
 
 int main() {
     int n, m;
     scanf("%d", &n);
     scanf("%d", &m);
-    getchar(); // consume newline character
 
-    char inputs[MAX_SIGNALS][MAX_SIGNAL_LENGTH + 1];
+    Input inputs[MAX_INPUTS];
     for (int i = 0; i < n; i++) {
-        scanf("%s", inputs[i]);
+        scanf("%s %s", inputs[i].name, inputs[i].signal);
+    }
+
+    Output outputs[MAX_INPUTS];
+    for (int i = 0; i < m; i++) {
+        scanf("%s %s %s %s", outputs[i].name, outputs[i].gate_type, outputs[i].input_name_1, outputs[i].input_name_2);
+        char* input_signal_1 = find_signal(inputs, n, outputs[i].input_name_1);
+        char* input_signal_2 = find_signal(inputs, n, outputs[i].input_name_2);
+        for (int j = 0; j < strlen(input_signal_1); j++) {
+            outputs[i].signal[j] = logic_gate(outputs[i].gate_type, input_signal_1[j], input_signal_2[j]);
+        }
     }
 
     for (int i = 0; i < m; i++) {
-        char output_name[MAX_SIGNAL_LENGTH + 1], gate_type, input_name_1[MAX_SIGNAL_LENGTH + 1], input_name_2[MAX_SIGNAL_LENGTH + 1];
-        char input_signal_1[MAX_SIGNAL_LENGTH + 1], input_signal_2[MAX_SIGNAL_LENGTH + 1], output_signal[MAX_SIGNAL_LENGTH + 1];
-
-        scanf("%s %c %s %s", output_name, &gate_type, input_name_1, input_name_2);
-
-        int index1, index2;
-        for (int j = 0; j < n; j++) {
-            if (strcmp(input_name_1, inputs[j]) == 0) index1 = j;
-            if (strcmp(input_name_2, inputs[j]) == 0) index2 = j;
-        }
-
-        strcpy(input_signal_1, inputs[index1]);
-        strcpy(input_signal_2, inputs[index2]);
-
-        for (int j = 0; input_signal_1[j] != '\0'; j++) {
-            output_signal[j] = logic_gate(gate_type, input_signal_1[j], input_signal_2[j]);
-        }
-        output_signal[strlen(input_signal_1)] = '\0';
-        
-        strcpy(inputs[i + n], output_signal);
-
-        printf("%s %s\n", output_name, output_signal);
+        printf("%s %s\n", outputs[i].name, outputs[i].signal);
     }
 
     return 0;
